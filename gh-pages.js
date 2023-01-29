15695ba211ad83fe30c95f82c55d06c965175420
arjunindia <arjun8107@gmail.com>
2023-01-29 22:03:03 +0530

    deploy

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 import { publish } from "gh-pages";
 
 publish(
-  "build", // path to public directory
+  "dist", // path to public directory
   {
     branch: "gh-pages",
     repo: "https://github.com/arjunindia/simple-CRUD-library-management.git", // Update to point to your repository
